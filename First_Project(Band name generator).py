3dcf7edaf0f28c255caf865e1e94786f76d71da7
SifatUllahKhan06 <ksifatullah06@gmail.com>
2025-10-05 12:41:14 +0530

    Add files via upload

---
+++ First_Project(Band name generator).py
@@ -0,0 +1,3 @@
+name = input("Hey Buddy! Welcome to my first project 'Band Name Generator'\nWhat's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your cute pet?\n")
+print("Your Brand Name is '" + name + pet_name + "'")
